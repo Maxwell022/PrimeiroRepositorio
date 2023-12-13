4edc6ccd35e34857d2722a1736794858a163e0f0
Maxwell022 <maxwelldragon@gmail.com>
2023-12-13 14:48:00 -0300

    3th

--- AgeCalcutator/Calculadora.py
+++ AgeCalcutator/Calculadora.py
@@ -4,50 +4,37 @@ from datetime import datetime as dt
 from tkinter import messagebox as mb
 
 def limpar():
-    lista = [campoNome, campoDia, campoMes, campoAno]
+    lista = [campoNome, campoAno]
 
     for input in lista:
         input.delete(0, tk.END)
 
 def getInput():
-    humano = Pessoa(campoNome.get(), dt(int(campoAno.get()), int(campoMes.get()), int(campoDia.get())))
-
-    mb.showinfo(title = "Resultado", message = humano.idade())
-
-def Calcular():
-    pass
-
-#
+    try:    
+        humano = Pessoa(campoNome.get(), int(campoAno.get()))
+        limpar()
+        mb.showinfo(title = "Resultado", message = f"Olá {humano.nome}, você tem {humano.idade()} anos de idade")
+    except ValueError:
+        mb.showerror(title = "Erro!", message = f"Informe apenas números no campo ano")
 
 janela = tk.Tk()
-janela.geometry("320x320")
+janela.geometry("280x180")
 janela.title("Age Calculator")
 
 nome = tk.Label(text = "Nome:", height = 2, font = ("comic sans", 14, "bold"))
 nome.grid(column = 0, row = 1)
 
-dia = tk.Label(text = "Dia:", height = 2, font = ("comic sans", 14, "bold"))
-dia.grid(column = 0, row = 2)
-
-mes = tk.Label(text = "Mês:", height = 2, font = ("comic sans", 14, "bold"))
-mes.grid(column = 0, row = 3)
 
 ano = tk.Label(text = "Ano:", height = 2, font = ("comic sans", 14, "bold"))
-ano.grid(column = 0, row = 4)
+ano.grid(column = 0, row = 2)
 
 #Entradas
 
 campoNome = tk.Entry(width = 12, font = ("comic sans", 14))
 campoNome.grid(column = 1, row = 1)
 
-campoDia = tk.Entry(width = 12, font = ("comic sans", 14))
-campoDia.grid(column = 1, row = 2)
-
-campoMes = tk.Entry(width = 12, font = ("comic sans", 14))
-campoMes.grid(column = 1, row = 3)
-
 campoAno = tk.Entry(width = 12, font = ("comic sans", 14))
-campoAno.grid(column = 1, row = 4)
+campoAno.grid(column = 1, row = 2)
 
 #Butões
 
